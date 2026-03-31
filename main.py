d3d64293208100d23c6074ae34283daef8fdf9b4
mitsib4l <up1093440@ac.upatras.gr>
2026-03-31 19:10:45 +0300

    for resume

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ def main():
     if args.resume:
         if os.path.isfile(args.resume):
             print("=> loading checkpoint '{}'".format(args.resume))
-            checkpoint = torch.load(args.resume)
+            checkpoint = torch.load(args.resume, weights_only=False)
             args.start_epoch = checkpoint['epoch']
             best_acc = checkpoint['best_acc']
             recorder = checkpoint['recorder']
