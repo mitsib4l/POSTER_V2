1d9d4fdc4e6c448ffeb5a5914196dd8177332447
mitsib4l <up1093440@ac.upatras.gr>
2026-04-02 13:09:20 +0300

    fix again

--- main.py
+++ main.py
@@ -181,9 +181,8 @@ def main():
     if args.evaluate is not None:
         if os.path.isfile(args.evaluate):
             print("=> loading checkpoint '{}'".format(args.evaluate))
-            checkpoint = torch.load(args.evaluate)
+            checkpoint = torch.load(args.evaluate, weights_only=False)
             best_acc = checkpoint['best_acc']
-            best_acc = best_acc.to()
             print(f'best_acc:{best_acc}')
             model.load_state_dict(checkpoint['state_dict'])
             print("=> loaded checkpoint '{}' (epoch {})".format(args.evaluate, checkpoint['epoch']))
